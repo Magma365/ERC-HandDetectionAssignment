efddba0d58470a0ba4e698c01b53fd5982f65b23
Magma365 <148254520+Magma365@users.noreply.github.com>
2023-11-10 16:23:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,72 @@
-import cv2
-import mediapipe as mp
+# Importing Libraries 
+import cv2 
+import mediapipe as mp 
 
-# Initialize MediaPipe Hand module
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands()
+# Used to convert protobuf message to a dictionary. 
+from google.protobuf.json_format import MessageToDict 
 
-# Capture video from webcam
-cap = cv2.VideoCapture(0)
+# Initializing the Model 
+mpHands = mp.solutions.hands 
+hands = mpHands.Hands( 
+	static_image_mode=False, 
+	model_complexity=1
+	min_detection_confidence=0.75, 
+	min_tracking_confidence=0.75, 
+	max_num_hands=2) 
 
-while True:
-    # Read a frame from the video capture
-    ret, frame = cap.read()
+# Start capturing video from webcam 
+cap = cv2.VideoCapture(0) 
 
-    # Convert the BGR image to RGB
-    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+while True: 
+	# Read video frame by frame 
+	success, img = cap.read() 
 
-    # Process the frame to get hand landmarks
-    results = hands.process(rgb_frame)
+	# Flip the image(frame) 
+	img = cv2.flip(img, 1) 
 
-    # Check if hand landmarks are available
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            # Extract landmarks for each hand
-            for lm_id, lm in enumerate(hand_landmarks.landmark):
-                # Access landmark coordinates (x, y, z)
-                x, y, z = int(lm.x * frame.shape[1]), int(lm.y * frame.shape[0]), lm.z
+	# Convert BGR image to RGB image 
+	imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
 
-                # Draw landmarks on the frame
-                cv2.circle(frame, (x, y), 8, (0, 255, 0), -1)
+	# Process the RGB image 
+	results = hands.process(imgRGB) 
 
-            # Determine hand orientation based on specific landmarks
-            if hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].x < hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_TIP].x:
-                hand_orientation = "Left Hand"
-            else:
-                hand_orientation = "Right Hand"
+	# If hands are present in image(frame) 
+	if results.multi_hand_landmarks: 
 
-            # Display hand orientation on the frame
-            cv2.putText(frame, hand_orientation, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+		# Both Hands are present in image(frame) 
+		if len(results.multi_handedness) == 2: 
+				# Display 'Both Hands' on the image 
+			cv2.putText(img, 'Both Hands', (250, 50), 
+						cv2.FONT_HERSHEY_COMPLEX, 
+						0.9, (0, 255, 0), 2) 
 
-    # Display the resulting frame
-    cv2.imshow("Hand Detection", frame)
+		# If any hand present 
+		else: 
+			for i in results.multi_handedness: 
+				
+				# Return whether it is Right or Left Hand 
+				label = MessageToDict(i) 
+				['classification'][0]['label'] 
 
-    # Break the loop if 'q' key is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+				if label == 'Left': 
+					
+					# Display 'Left Hand' on 
+					# left side of window 
+					cv2.putText(img, label+' Hand', 
+								(20, 50), 
+								cv2.FONT_HERSHEY_COMPLEX, 
+								0.9, (0, 255, 0), 2) 
 
-# Release the video capture object
-cap.release()
+				if label == 'Right': 
+					
+					# Display 'Left Hand' 
+					# on left side of window 
+					cv2.putText(img, label+' Hand', (460, 50), 
+								cv2.FONT_HERSHEY_COMPLEX, 
+								0.9, (0, 255, 0), 2) 
 
-# Close all OpenCV windows
-cv2.destroyAllWindows()
+	# Display Video and when 'q' 
+	# is entered, destroy the window 
+	cv2.imshow('Image', img) 
+	if cv2.waitKey(1) & 0xff == ord('q'): 
+		break
