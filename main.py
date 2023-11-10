204812cc1f1a09bda4617004733662a9367f17a8
Magma365 <148254520+Magma365@users.noreply.github.com>
2023-11-10 17:00:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,67 +1,93 @@
+# Import Libraries
 import cv2
-import mediapipe as mp
-from google.protobuf.json_format import MessageToDict
 
-# Initializing the MediaPipe Hands model
-mpHands = mp.solutions.hands
-hands = mpHands.Hands(
-    static_image_mode=False,
-    model_complexity=1,
-    min_detection_confidence=0.75,
-    min_tracking_confidence=0.75,
-    max_num_hands=2
-)
+# Print OpenCV version
+print(cv2.__version__)
 
-# Start capturing video from the webcam
-cap = cv2.VideoCapture(0)
+# Define the mpHands Class
+class mpHands:
+    # Import mediapipe as mp
+    import mediapipe as mp
 
+    # Constructor to initialize the Hands object
+    def __init__(self, maxHands=2, tol1=.5, tol2=.5):
+        # Create Hands object with specified parameters
+        self.hands = self.mp.solutions.hands.Hands(False, maxHands, tol1, tol2)
+
+    # Method to extract hand landmarks and types from a frame
+    def Marks(self, frame):
+        # Lists to store hand landmarks and types
+        myHands = []
+        handsType = []
+
+        # Convert the frame to RGB
+        frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # Process the frame using the hands object
+        results = self.hands.process(frameRGB)
+
+        # Check if hands are detected in the frame
+        if results.multi_hand_landmarks != None:
+            # Loop over detected hands and their types
+            for hand in results.multi_handedness:
+                handType = hand.classification[0].label
+                handsType.append(handType)
+
+            # Loop over landmarks in each hand
+            for handLandMarks in results.multi_hand_landmarks:
+                myHand = []
+
+                # Extract (x, y) coordinates of each landmark and append to myHand
+                for landMark in handLandMarks.landmark:
+                    myHand.append((int(landMark.x * width), int(landMark.y * height)))
+
+                # Append the list of landmarks for the hand to myHands
+                myHands.append(myHand)
+
+        # Return the collected hand landmarks and types
+        return myHands, handsType
+
+# Set video parameters
+width = 1280
+height = 720
+
+# Open video capture with specified parameters
+cam = cv2.VideoCapture(4, cv2.CAP_DSHOW)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+cam.set(cv2.CAP_PROP_FPS, 30)
+cam.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+
+# Instantiate mpHands object with maxHands set to 2
+findHands = mpHands(2)
+
+# Main loop for video capture and hand detection
 while True:
-    # Read video frame by frame
-    success, img = cap.read()
+    # Read a frame from the video capture
+    ignore, frame = cam.read()
 
-    # Flip the image (frame)
-    img = cv2.flip(img, 1)
+    # Resize the frame to specified width and height
+    frame = cv2.resize(frame, (width, height))
 
-    # Convert BGR image to RGB image
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # Call Marks method to get hand landmarks and types
+    handData, handsType = findHands.Marks(frame)
 
-    # Process the RGB image
-    results = hands.process(imgRGB)
+    # Loop over detected hands and draw circles at specific landmarks
+    for hand, handType in zip(handData, handsType):
+        # Determine hand color based on hand type
+        handColor = (255, 0, 0) if handType == 'Right' else (0, 0, 255)
 
-    # If hands are present in the image (frame)
-    if results.multi_hand_landmarks:
+        # Draw circles at specific landmarks
+        for ind in [0, 5, 6, 7, 8]:
+            cv2.circle(frame, hand[ind], 15, handColor, 5)
 
-        # Both Hands are present in the image (frame)
-        if len(results.multi_handedness) == 2:
-            # Display 'Both Hands' on the image
-            cv2.putText(img, 'Both Hands', (250, 50),
-                        cv2.FONT_HERSHEY_COMPLEX,
-                        0.9, (0, 255, 0), 2)
+    # Display the frame
+    cv2.imshow('my WEBcam', frame)
+    cv2.moveWindow('my WEBcam', 0, 0)
 
-        # If any hand is present
-        else:
-            for i in results.multi_handedness:
-                # Return whether it is Right or Left Hand
-                label = MessageToDict(i)['classification'][0]['label']
-
-                if label == 'Left':
-                    # Display 'Left Hand' on the left side of the window
-                    cv2.putText(img, label + ' Hand',
-                                (20, 50),
-                                cv2.FONT_HERSHEY_COMPLEX,
-                                0.9, (0, 255, 0), 2)
-
-                if label == 'Right':
-                    # Display 'Right Hand' on the right side of the window
-                    cv2.putText(img, label + ' Hand', (460, 50),
-                                cv2.FONT_HERSHEY_COMPLEX,
-                                0.9, (0, 255, 0), 2)
-
-    # Display the video, and when 'q' is entered, destroy the window
-    cv2.imshow('Image', img)
+    # Break the loop if 'q' key is pressed
     if cv2.waitKey(1) & 0xff == ord('q'):
         break
 
-# Release the capture and close all windows
-cap.release()
-cv2.destroyAllWindows()
+# Release the video capture
+cam.release()
